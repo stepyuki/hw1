4cb8912b4b7b475b442a8ead4968b645161376d1
stepyuki <39672325+stepyuki@users.noreply.github.com>
2018-05-29 18:29:24 +0900

    second

--- wordsearch.py
+++ wordsearch.py
@@ -5,7 +5,10 @@ ld.close()
 
 print("Enter word")
 ls = sys.stdin.readline()
-searchword = ''.join(ls.splitlines())
+word = sorted(''.join(ls.splitlines()))
+searchword = ''.join(word)
+print searchword
+print("Search Result")
 for line in lines:
     if line.find(searchword) >= 0:
         word = line[:-1].split(",")
