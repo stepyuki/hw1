06d7cf8c13c5454ca18ea8a54997cd4be9afafd5
stepyuki <HiKuuyi@gmail.com>
2018-06-01 00:43:03 +0900

    submission

--- enteranagram.py
+++ enteranagram.py
@@ -1,16 +1,29 @@
+#coding:utf-8
+#入力された文字列の組み合わせを列挙しファイルに書き込む
 import sys
 import itertools
 f = open("Search.txt","w")
 print("Enter word")
+
+#文字を入力
 ls = sys.stdin.readline()
 word = sorted(''.join(ls.splitlines()))
 anagram = []
-for j in range(4,len(word)):
+#qをquにする
+for letter in word:
+    if letter == 'q':
+        word.append("qu")
+        word.remove("u")
+        word.remove("q")
+
+#組み合わせを出力
+for j in range(1,len(word)):
     for word_combi in itertools.combinations(word,j):
         anagram.append(''.join(word_combi))
 anagram_set = sorted(set(anagram),key=anagram.index)
 for word_write in anagram_set:
-    f.write(word_write+"\n")
+    word_sep = sorted(list(word_write))
+    f.write(''.join(word_sep)+"\n")
 f.close()
         
         
--- wordsearch.py
+++ wordsearch.py
@@ -1,7 +1,10 @@
+#coding:utf-8
 import sys
 import itertools
 import bisect
+#辞書のファイル,1列目がアルファベット順,2列目はもとの単語
 f = open("dict3.txt","r")
+#検索する文字列の組み合わせ
 f2 = open("Search.txt","r")
 lines = f.readlines()
 search = f2.readlines()
@@ -9,7 +12,9 @@ f.close()
 f2.close()
 point_mvp = 0
 word_mvp = []
+vowel_min = 0
 print("Search Result")
+#単語が存在しているか
 for word in search:
     search_str = word.strip() + ","
     top_point = bisect.bisect_left(lines,search_str)
@@ -18,6 +23,8 @@ for word in search:
     if len(result) != 0:
         result2 = result[0].split(",")
         point = 0
+        vowel = 0
+        #点数計算
         for letter in result2[0]:
             if letter in ['c','f','h','l','m','p','v','w','y','q']:
                 point += 2
@@ -25,8 +32,14 @@ for word in search:
                 point += 3
             else:
                 point += 1
-        print(result2[1]+"--"+str(point))
-        if point_mvp < point:
+            #あまり母音を使わないようにする
+            if letter in ['a','i','u','e','o']:
+                vowel += 1
+        print result2[1].strip(),
+        if point_mvp < point or point_mvp == point and vowel_min > vowel:
             point_mvp = point
             word_mvp = result2[1]
-print("BEST:"+str(word_mvp))
+            vowel_min = vowel
+#一番良いものを出力
+print("\nBEST:"+str(word_mvp))
+print("POINT:"+str(point_mvp))
